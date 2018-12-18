756d51cc579b2088bc2978fb35fd790688d204ae
Kitlith <kitlith@kitl.pw>
2018-12-17 19:52:57 -0800

    Bump Version

--- oldboosters_config/src/main/java/pw/kitl/oldboosters/OldBoostersMod.java
+++ oldboosters_config/src/main/java/pw/kitl/oldboosters/OldBoostersMod.java
@@ -17,7 +17,7 @@ import pw.kitl.oldboosters.OldBoostersConfig;
 public class OldBoostersMod {
     public static final String MODID = "oldboosters";
     public static final String NAME = "Old Boosters Mod";
-    public static final String VERSION = "1.0";
+    public static final String VERSION = "1.1";
 
     // private static Logger logger;
 
